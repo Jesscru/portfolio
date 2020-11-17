2ce788fc1c439301b9f51cb33c36fcff9932b870
jesscru <jessicacrump7@gmail.com>
2020-11-16 21:02:31 -0700

    added a border to the email-icon image on contact page

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 $( document ).ready(function() {
 var emailIcon = $('.email-icon');
 var form = $('.form');
+var border = $('.border');
 
 
     emailIcon.on('click', function(){
         form.css('visibility', 'visible');
         emailIcon.css('visibility', 'hidden');
+        border.css('visibility', 'hidden');
     })
 
 
