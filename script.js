164f0e0723a7a3da467f2d840cb8a5f4fc9cf13b
jesscru <jessicacrump7@gmail.com>
2020-11-18 00:45:20 -0700

    added resume information to add to page on mousover event and styled it

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 $( document ).ready(function() {
-var messageIcon = $('.message-icon');
-var emailIcon = $('.email-icon');
-var form = $('.form');
-var contactLabels = $('.find-me');
+
+const messageIcon = $('.message-icon');
+const emailIcon = $('.email-icon');
+const form = $('.form');
+const contactLabels = $('.find-me');
+const populateInfo = $('.populate-info');
+
 
 
     messageIcon.on('click', function(){
@@ -13,5 +16,53 @@ var contactLabels = $('.find-me');
         contactLabels.css('visibility', 'hidden');
     })
 
+const resumeInfo = {
+    skills: {
+        general: ['web development', 'attentive to detail', 'customer service', 'team collaboration', 'teaching and training', 'malleable and adaptable', 'handling confidential information', 'conversational in Spanish'],
+        codingSpecific: ['HTML', 'CSS', 'Javascript']
+    },
 
+     workExperience: {
+        'Peace Corps': 'Taught English as a second language to Ecuadorian high school students',
+        'Restaurant manager': 'Trained numerous employees, delegated work duties, counted and balanced cash register',
+        'U of Miss Tutor': 'Provided Spanish help and practice to university students'
+    }, 
+
+    leadershipOther: {
+        'Reasearch assistant': 'Worked with a team to study cognitive function in children; handled data collection and entry',
+        'President of Sport Club Council': 'Collaborated with council members to make decisions about the organization as a whole as well as decisions about each sport club',
+        'Vice president of Wellness Ambassadors': 'Assisted each of 4 wellness subgroups in running their programs and events'
+    }, 
+
+    education: {
+        university: 'University of Mississippi', 
+        years: 'August 2015-May 2019',
+        major: 'B.A. Psychology', 
+        minors: 'Spanish, chemistry, biology',
+        GPA: 3.74
+    }
+}
+    $("#skills").mouseover(function() {
+        let newPar = $('<p class="populated-skills">')
+        newPar.html(`${resumeInfo.skills.general}`)
+        populateInfo.append(newPar);
+    })
+
+    $("#experience").mouseover(function() {
+        let newPar = $('<p class="populated-experience">')
+        newPar.html(`Reasearch assistant: ${resumeInfo.workExperience["Peace Corps"]}
+        Restaurant manager: ${resumeInfo.workExperience["Restaurant manager"]}
+        U of Miss Tutor: ${resumeInfo.workExperience["U of Miss Tutor"]}`)
+        populateInfo.append(newPar);
+    })
+
+    $("#education").mouseover(function() {
+        let newPar = $('<p class="populated-education">')
+        newPar.html(`${resumeInfo.education.university}
+        ${resumeInfo.education.years}
+        Major: ${resumeInfo.education.major}
+        Minors: ${resumeInfo.education.minors}
+        GPA: ${resumeInfo.education.GPA}`)
+        populateInfo.append(newPar);
+    })
 })
